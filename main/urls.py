553cb6f3cb55a4f95ccb76e5628fc0fe7298b6bf
Vivek17020 <116427464+Vivek17020@users.noreply.github.com>
2023-12-12 22:16:14 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('create-post/', views.create_post,name="create-post"),
     path('update-post/<int:post_id>/', views.update_post, name='update-post'),
     path('delete-post/', views.delete_post, name='delete-post'),
-    path('api/generate-product-description/', views.generate_product_description, name='generate-product-description'),
+    path('ImageAnalyzer/', ImageAnalyzer.home,name="home"),
     path('api/image-recognition/', views.image_recognition, name='image-recognition'),
 ]
